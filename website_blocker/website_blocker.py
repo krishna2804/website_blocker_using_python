f60b8d6b15a90bc36dd7aa5a7bc84c8dfa8b96d8
krishna2804 <pamuri1997@gmail.com>
2018-07-09 14:18:33 +0530

    simple website blocker using python

---
+++ website_blocker/website_blocker.py
@@ -0,0 +1,31 @@
+import time
+from datetime import datetime as dt
+
+hosts_temp=r"F:\Desktop\5sem\python\project_2\hosts"
+hosts_path=r"C:\Windows\System32\drivers\etc\hosts"
+redirect="127.0.0.1"
+website_list=["www.facebook.com","www.gmail.com","github.com"]
+
+while True:
+    if dt(dt.now().year,dt.now().month,dt.now().day,10) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,19):
+        print("work hors.....")
+        with open(hosts_temp,'r+') as file:
+            content=file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect+" "+ website+"\n")
+        
+    else:
+        with open(hosts_temp,'r+') as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        
+        print("fun hours...")
+    time.sleep(8)
+    
